e4c06a7a41a85efaefad99b58b4594ea55a7b94a
Im-loki <al4126@naver.com>
2023-09-01 17:33:43 +0900

    SignUp 추가

---
+++ randomChat/login/SignUpViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SignUpViewController.swift
+//  randomChat
+//
+//  Created by 양주원 on 2023/08/31.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBAction func ExitSignUpButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+}
